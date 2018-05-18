cf90ac975302e387671ba686761e45651d7d5cfa
Lord Sahvith <bulletproofchild@gmail.com>
2018-05-18 16:06:56 -0600

    Add readme.md

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+module.exports = Phrase;
+
 // add `reverse` to all strings
 String.prototype.reverse = function() {
     return Array.from(this).reverse().join("");
-}
-
-
-// exercise 7.3
-String.prototype.blank = function blank() {
-    let blank;
-    if (this.match(/^[\s+\n+]/)) {
-        blank = true;
-    } else {
-        blank = false;
-    }
-    return blank;
 }
 
 // Defines a Phrase Object
@@ -33,27 +23,4 @@ function Phrase(content) {
     this.palindrome = function palindrome() {
         return this.processedContent() === this.processedContent().reverse();
     }
-    
-    // exercise 7.1
-    // makes the phrase LOUDER
-    this.louder = function louder() {
-        return this.content.toUpperCase();
-    }
-    
-    // exercise 7.3
-    this.blankPalindrome = function blankPalindrome() {
-        return this.content.blank();
-    }
 }
-
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-    
-    // Returns translation processed for palindrome testing
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
-    }
-}
-
-TranslatedPhrase.prototype = new Phrase();
