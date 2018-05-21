0f160225475a69c727c61d9960e182d5e46a07b1
Lord Sahvith <bulletproofchild@gmail.com>
2018-05-21 14:49:19 -0600

    Check for empty string and return false

--- index.js
+++ index.js
@@ -17,19 +17,14 @@ function Phrase(content) {
     // Returns the letters in the content.
     this.letters = function letters() {
         return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
-//        let theLetters = [];
-//        const letterRegEx = /[a-z]/i;
-//        Array.from(this.content).forEach(function(charater) {
-//           if (charater.match(letterRegEx)) {
-//               theLetters.push(charater);
-//           } 
-//        });
-//        
-//        return theLetters.join("");
     }
 
     // Returns true for a plaindrome, false otherwise
     this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
+        if (this.letters()) {
+          return this.processedContent() === this.processedContent().reverse();
+        } else {
+          return false;
+        }
     }
 }
